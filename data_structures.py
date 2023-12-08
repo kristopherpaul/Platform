8e417870934aad1be579660177e52bc04193fe70
Arjit7d3 <129239596+Arjit7d3@users.noreply.github.com>
2023-12-08 11:51:33 +0530

    Update data_structures.py
    bug fixes, removed some incorrect code, TODOs mentioned

--- data_structures.py
+++ data_structures.py
@@ -5,10 +5,14 @@ Time Vector is always a series of values indexed by timestamps
 """
 
 """
-TODO: implement custom time class
+1. TODO: type hint for everything
+2. TODO: use @overload wherever necessary
+3. TODO: implement binary search for indexing using datetime
+4. TODO: write code for error handling wherever necessary
 """
 
 import datetime
+from typing import overload
 import numpy as np
 
 
@@ -26,7 +30,7 @@ class TimeArray:
     def __len__(self):
         return len(self.values)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int):
         """
         TODO: implement for slice
         format of time is (year, month, day, hour, min, sec)
@@ -34,52 +38,49 @@ class TimeArray:
         if isinstance(i, int):
             return self.values[i]
         if isinstance(i, datetime.datetime):
-            i = np.datetime64(i, 'm')
-            if ((i - self.begin) % self.step):
-                # TODO: ERORR
-                return -1
+            # TODO: Implement this, use binary search
+            pass
 
-            index = (i - self.begin) // self.step
-            return self.values[index]
-
-    def getRange(self, start, end):
-        """
-        format of start and end is (year, month, day, hour, min, sec) [inclusive]
-        """
-        start = np.datetime64(start)
-        end = np.datetime64(end)
-        if (((start - self.begin) % self.step) > 0) and (((end - self.begin) % self.step) != 0):
-            # TODO: ERROR
-            return -1
-
-        
-        start_index = max(0, (start - self.begin) // self.step)
-        end_index = min(len(self.values) - 1, (end - self.begin) // self.step)
-        return TimeArray(self.values[start_index:end_index + 1], self.timestamps[start_index:end_index + 1])
-    
     def __repr__(self):
-        # TODO: fix if the length of the dataset is less than 10
         ret = "Time \t\t\t Value"
-        for i in range(5):
-            ret += '\n'
-            ret += f"{np.datetime_as_string(self.timestamps[i], 'm').replace('T', ' ')} \t {self.values[i]}"
-        ret += '\n... \t\t\t ...'
-        for i in range(len(self.values) - 5, len(self.values)):
-            ret += '\n'
-            ret += f"{np.datetime_as_string(self.timestamps[i], 'm').replace('T', ' ')} \t {self.values[i]}"
+
+        if len(self.values) <= 15:
+            for i in range(len(self.values)):
+                ret += '\n'
+                ret += f"{np.datetime_as_string(self.timestamps[i], 'm').replace('T', ' ')} \t {self.values[i]}"
+        else:
+            for i in range(5):
+                ret += '\n'
+                ret += f"{np.datetime_as_string(self.timestamps[i], 'm').replace('T', ' ')} \t {self.values[i]}"
+            ret += '\n... \t\t\t ...'
+            for i in range(len(self.values) - 5, len(self.values)):
+                ret += '\n'
+                ret += f"{np.datetime_as_string(self.timestamps[i], 'm').replace('T', ' ')} \t {self.values[i]}"
         
         return ret
+    
+    def __sub__(self, other):
+        # TODO: handle errors when self and other is not compatible
+        ret_values = []
+        for i in range(len(self)):
+            ret_values.append(self[i] - other[i])
+        return TimeArray(ret_values, self.timestamps)
+
 
 class TimeMatrix:
+
     def __init__(self, data):
         """ 
         data must have a time column indexed by 'time'
         """
         self.time = np.array(data['date'], dtype='datetime64[m]')
         self.open = TimeArray(data['open'], self.time)
-        self.high = TimeArray(data['open'], self.time)
-        self.low = TimeArray(data['open'], self.time)
-        self.close = TimeArray(data['open'], self.time)
+        self.high = TimeArray(data['high'], self.time)
+        self.low = TimeArray(data['low'], self.time)
+        self.close = TimeArray(data['close'], self.time)
         self.begin = self.time[0]
         self.end = self.time[-1]
         self.step = self.time[1] - self.time[0]
+
+    def __len__(self):
+        return len(self.open)
